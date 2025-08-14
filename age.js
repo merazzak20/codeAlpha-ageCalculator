8065636579597fe5e5b6d76f990007276a68c84f
merazzak <info.merazzak@gmail.com>
2025-08-14 23:40:39 +0600

    update code

--- age.js
+++ age.js
@@ -1,6 +1,7 @@
 const age = "10 / 5 / 18";
 
 const today = new Date();
+const birthday = new Date(year, month - 1, day);
 
 const currentDate = today.getDate();
 const currentMonth = today.getMonth() + 1;
