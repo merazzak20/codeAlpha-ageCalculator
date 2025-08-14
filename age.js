e0d6a3e733c0ab75bc496f9c78cd40894f929d5b
merazzak <info.merazzak@gmail.com>
2025-08-15 00:14:58 +0600

    implement html

--- age.js
+++ age.js
@@ -1,10 +1,20 @@
-const age = "10 / 5 / 18";
+// console.log(year);
 
-const today = new Date();
-const birthday = new Date(year, month - 1, day);
+const calculateAge = () => {
+  const year = document.querySelector("#year").value;
+  const month = document.querySelector("#month").value;
+  const day = document.querySelector("#day").value;
 
-const currentDate = today.getDate();
-const currentMonth = today.getMonth() + 1;
-const currentYear = today.getFullYear();
+  const today = new Date();
+  const birthday = new Date(year, month - 1, day);
 
-console.log(today, currentDate, currentMonth, currentYear);
+  const currentDate = today.getDate();
+  const currentMonth = today.getMonth() + 1;
+  const currentYear = today.getFullYear();
+
+  const ageYear = currentYear - birthday.getFullYear();
+  const ageMonth = currentMonth - birthday.getMonth();
+  const ageDay = currentDate - birthday.getDate();
+
+  console.log(ageYear, ageMonth, ageDay);
+};
