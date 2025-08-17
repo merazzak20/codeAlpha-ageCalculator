88192d2ed33f5a634f0fd419e622a009b2f6e87f
merazzak <info.merazzak@gmail.com>
2025-08-17 22:20:01 +0600

    complete the code

--- age.js
+++ age.js
@@ -1,20 +1,32 @@
-// console.log(year);
-
 const calculateAge = () => {
   const year = document.querySelector("#year").value;
   const month = document.querySelector("#month").value;
   const day = document.querySelector("#day").value;
 
+  if (!day || !month || !year) {
+    document.getElementById("result").innerText =
+      "Please enter full date of birth!";
+    return;
+  }
+
   const today = new Date();
   const birthday = new Date(year, month - 1, day);
 
-  const currentDate = today.getDate();
-  const currentMonth = today.getMonth() + 1;
-  const currentYear = today.getFullYear();
+  let ageYears = today.getFullYear() - birthday.getFullYear();
+  let ageMonths = today.getMonth() - birthday.getMonth();
+  let ageDays = today.getDate() - birthday.getDate();
 
-  const ageYear = currentYear - birthday.getFullYear();
-  const ageMonth = currentMonth - birthday.getMonth();
-  const ageDay = currentDate - birthday.getDate();
+  if (ageDays < 0) {
+    ageMonths--;
+    ageDays += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+  }
 
-  console.log(ageYear, ageMonth, ageDay);
+  if (ageMonths < 0) {
+    ageYears--;
+    ageMonths += 12;
+  }
+
+  document.getElementById(
+    "result"
+  ).innerText = `Your Age: ${ageYears} years, ${ageMonths} months, ${ageDays} days`;
 };
